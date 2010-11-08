032f5cdf5c0d742c9e4d9669d46a7c546c21da49
julien <julien.genestoux@gmail.com>
2010-11-08 21:47:39 +0100

    adding validation when adding feeds

---
+++ validate_doc_update/feed.js
@@ -0,0 +1,5 @@
+function(newDoc, oldDoc, context){
+  if (newDoc.type == "feed") {
+    // throw {"forbidden" : "no matter what"} // We may want to throw errors for duplicate feeds, but it's not necessary.
+  }
+}
